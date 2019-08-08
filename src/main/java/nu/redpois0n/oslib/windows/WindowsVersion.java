6ccdac95e564666bd4f7947a8fef97a42f41b907
Himanshu Shekhar <himanshub16@users.noreply.github.com>
2019-08-08 20:44:52 +0530

    detect recent windows server versions and fallback on unknown

--- src/main/java/nu/redpois0n/oslib/windows/WindowsVersion.java
+++ src/main/java/nu/redpois0n/oslib/windows/WindowsVersion.java
@@ -4,17 +4,21 @@ import nu.redpois0n.oslib.VersionCompare;
 
 public enum WindowsVersion implements VersionCompare {
 
-    UNKNOWN("Unknown Windows"),
     WIN2000("Windows 2000"),
     WINXP("Windows XP"),
     WINSERVER2003("Windows 2003"), // Note that Windows Server 2003 identifies itself only as Windows 2003
     WINVISTA("Windows Vista"),
     WINSERVER2008("Windows Server 2008"),
+    WINSERVER2008R2("Windows Server 2008 R2"),
     WIN7("Windows 7"),
     WIN81("Windows 8.1"),
     WIN8("Windows 8"),
     WINSERVER2012("Windows Server 2012"),
-    WIN10("Windows 10");
+    WINSERVER2012R2("Windows Server 2012 R2"),
+    WIN10("Windows 10"),
+    WINSERVER2016("Windows Server 2016"),
+    WINSERVER2019("Windows Server 2019"),
+    UNKNOWN(System.getProperty("os.name"));   // if we are here, we can confidently use this
 
     private final String search;
     private final String version;
